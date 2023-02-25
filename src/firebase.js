e38fe3ebb7a0cc73725177db8a083373c8ecea24
arlequine <arlequine666.ah@gmail.com>
2023-02-25 10:49:07 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5Wmpc4r-nmuqaFTizU3ah8XnD14gOdOQ",
-  authDomain: "crud-example-react.firebaseapp.com",
-  projectId: "crud-example-react",
-  storageBucket: "crud-example-react.appspot.com",
-  messagingSenderId: "904910679726",
-  appId: "1:904910679726:web:22d7f66a64417b8fdec73f"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
